8cd05f9293b10d170514a7eb7ec36d65a84f4eee
ohads <ohad.sharet@gmcil.com>
2022-04-04 20:19:23 +0300

    first commit

---
+++ part2/part2.test.ts
@@ -0,0 +1,84 @@
+import { countLetters, isPaired, treeToSentence, WordTree } from "../../src/part2/part2";
+
+describe("Assignment 1 Part 2", () => {
+    describe("countLetters", () => {
+        it("counts letters", () => {
+            expect(countLetters("aaabbbb")).toEqual({"a": 3, "b":4});
+            expect(countLetters("AaaBbbb")).toEqual({"a": 3, "b":4});
+            expect(countLetters("ABbbaab")).toEqual({"a": 3, "b":4});
+            expect(countLetters("I am robot")).toEqual({"i": 1, "a": 1, "m": 1, "r":1, "o":2, "b":1, "t":1});
+        });
+    });
+
+    describe("Assignment 1 Part 2 ohad extra tests", () => {
+        describe("countLetters", () => {
+            it("counts letters", () => {
+                expect(countLetters("i i i i i")).toEqual({"i": 5});
+                expect(countLetters("#@$^%&%^*)%&")).toEqual({});
+                expect(countLetters("                          ....      ")).toEqual({});
+                expect(countLetters("O!H@A#d")).toEqual({"o": 1,"a": 1,"h": 1,"d": 1});
+            });
+        });
+    });
+    describe("isPaired", () => {
+        it("returns true for a string with paired parens", () => {
+            expect(isPaired("([{}])")).toBe(true);
+            expect(isPaired("This is ([some]) {text}.")).toBe(true);
+            expect(isPaired("No parens, no problems.")).toBe(true);
+            expect(isPaired("")).toBe(true);
+        });
+
+        it("ohad positiv testing", () => {
+            expect(isPaired("(((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((([{}]))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))")).toBe(true);
+            expect(isPaired("()")).toBe(true);
+            expect(isPaired("{(dsadsad)()asdasdasd(sadasd)()asdasdas}")).toBe(true);
+            expect(isPaired("[()]{}{[()()]()}")).toBe(true);
+            
+        });
+
+        it("returns false when the parens are not paired", () => {
+            expect(isPaired("(]")).toBe(false);
+            expect(isPaired("This is ]some[ }text{")).toBe(false);
+            expect(isPaired("(")).toBe(false);
+            expect(isPaired(")(")).toBe(false);
+
+        });
+
+        it("ohad negativ tests", () => {
+            expect(isPaired("({)}")).toBe(false);
+            expect(isPaired("(((()))")).toBe(false);
+            expect(isPaired("(()))")).toBe(false);
+            expect(isPaired("][")).toBe(false);
+            expect(isPaired("}{")).toBe(false);
+            
+        });
+    });
+
+    describe("treeToSentence", () => {
+        const t1: WordTree = {root:"hello", children:[{root: "world", children:[]}]}
+        const t2: WordTree = {root:"hello", children:[{root: "there", children:[]}, {root:"!", children:[]}]}
+        const t3: WordTree = {root:"hello", children:[{root: "there", children:[{root:"!", children:[]}]}]}
+        it("Represents a tree as a sentence", () => {
+            expect(treeToSentence(t1)).toBe("hello world");
+            expect(treeToSentence(t2)).toBe("hello there !");
+            expect(treeToSentence(t3)).toBe("hello there !");
+
+        });
+    });
+
+    describe("treeToSentence- Ohad", () => {
+        const t2: WordTree = {root:"hello", children:[{root: "students",
+         children:[{root:"how", children:[]}]}, {root:"are", children:[]},{root:"you", children:[]}]}
+        const t3: WordTree = {root:"barad", children:[{root: "yarad", children:[{root:"bedrom", children:[]}]},
+        {root: "sfarad", children:[{root: "!!!", children:[]}]} ]}
+        const t4: WordTree = {root:"1", children:[{root: "2", children:[{root:"3", children:[]}]},
+        {root: "4", children:[{root: "5", children:[]}]},{root: "6", children:[{root: "7", children:[]}]} ]}
+        it("Represents a tree as a sentence", () => {
+            expect(treeToSentence(t2)).toBe("hello students how are you");
+            expect(treeToSentence(t3)).toBe("barad yarad bedrom sfarad !!!");
+            expect(treeToSentence(t4)).toBe("1 2 3 4 5 6 7");
+        });
+    });
+});
+
+
---
+++ part3/find.test.ts
@@ -0,0 +1,87 @@
+import * as R from "../../src/lib/result";
+import * as F from "../../src/part3/find";
+
+describe("Find", () => {
+    describe("findResult", () => {
+        it("returns a Failure when no element was found", () => {
+            const result = F.findResult(x => x.length > 3, ["dog", "cat", "rat"]);
+            expect(result).toSatisfy(R.isFailure);
+        });
+
+        it("returns an Ok when an element was found", () => {
+            const result = F.findResult(x => x.length > 3, ["raccoon", "ostrich", "slug"]);
+            expect(result).toSatisfy(R.isOk);
+        });
+    });
+
+    describe("Find - ohad", () => {
+        describe("findResult", () => {
+            it("returns a Failure when no element was found numbers - ohad", () => {
+                const result = F.findResult(x => x > 3, [0,1,2]);
+                expect(result).toSatisfy(R.isFailure);
+            });
+    
+            it("returns an Ok when an element was found numbers - ohad", () => {
+                const result = F.findResult(x => x > 3, [1,2,3,4]);
+                expect(result).toSatisfy(R.isOk);
+            });
+        });
+    });
+    describe("returnSquaredIfFoundEven", () => {
+        it("returns an Ok of the first even number squared in v2", () => {
+            const result = F.returnSquaredIfFoundEven_v2([1, 2, 3]);
+            expect(result).toEqual(R.makeOk(4));
+        });
+
+        
+        it("return a Failure if no even numbers are in the array in v2", () => {
+           const result = F.returnSquaredIfFoundEven_v2([1, 3, 5]);
+           expect(result).toSatisfy(R.isFailure);
+        });
+
+        it("returns the first even number squared in v3", () => {
+            expect(F.returnSquaredIfFoundEven_v3([1, 2, 3])).toEqual(4);
+        });
+
+        it("returns -1 if no even numbers are in the array in v3", () => {
+            expect(F.returnSquaredIfFoundEven_v3([1, 3, 5])).toEqual(-1);
+        });
+    });
+
+    describe("returnSquaredIfFoundEven - ohad", () => {
+        it("returns an Ok of the first even number squared in v2 Fractures", () => {
+            const result = F.returnSquaredIfFoundEven_v2([1.0, 2.0, 3.0]);
+            expect(result).toEqual(R.makeOk(4));
+        });
+
+        it("returns an Ok of the first even number squared in v2 diffrent numbers", () => {
+            const result = F.returnSquaredIfFoundEven_v2([55,22,13]);
+            expect(result).toEqual(R.makeOk(484));
+        });
+
+        it("returns an Ok of the first even number squared in v2 with 0", () => {
+            const result = F.returnSquaredIfFoundEven_v2([0]);
+            expect(result).toEqual(R.makeOk(0));
+        });
+
+        
+        it("return a Failure if no even numbers are in the array in v2 diffrent numbers", () => {
+           const result = F.returnSquaredIfFoundEven_v2([11, 35, 57]);
+           expect(result).toSatisfy(R.isFailure);
+        });
+
+        it("return a Failure if no even numbers are in the array in v2 Fractures", () => {
+            const result = F.returnSquaredIfFoundEven_v2([0.5 , 2.5 , 4.5]);
+            expect(result).toSatisfy(R.isFailure);
+         });
+
+        it("returns the first even number squared in v3", () => {
+            expect(F.returnSquaredIfFoundEven_v3([1, 2, 3])).toEqual(4);
+        });
+
+        it("returns -1 if no even numbers are in the array in v3", () => {
+            expect(F.returnSquaredIfFoundEven_v3([1, 3, 5])).toEqual(-1);
+        });
+    });
+});
+
